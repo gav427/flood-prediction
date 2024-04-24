02f57377fd84c426ded271186b06b27de4c5aac8
Gavin Chapman-Moore <gavin.chapmanmoore@gmail.com>
2024-04-24 21:48:39 +1000

    ./model.py: converting data to ndarrays + moving normalisation above data splitting

--- src/model.py
+++ src/model.py
@@ -5,17 +5,33 @@ df_rain = pd.read_csv("../data/chennai-monthly-rains.csv")
 df_flood = pd.read_csv("../data/chennai-monthly-manual-flood.csv")
 
 # clean data
-X = df_rain.drop(columns=['Total'])
-y = df_flood[(df_flood.year <= 2021)]
+df_rain = df_rain.drop(columns=['Total'])
+df_flood = df_flood[(df_flood.year <= 2021)]
 
 import matplotlib.pyplot as plt
 
 # visualise data
-X.plot(x="Year")
-y.plot(x="year")
-#plt.show()
+df_rain.plot(x="Year")
+df_flood.plot(x="year")
+plt.show()
+
+# convert to arrays
+X = df_rain.drop(columns="Year").values
+y = df_flood.values
 
 ### TODO: should data be balanced here? How can mostly false (0) be balanced?
+
+from sklearn.preprocessing import MinMaxScaler
+
+# normalise data
+#   TODO: replace sklearn with tf
+#         retain year coloumn, months if possible
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X) #scaler.fit_transform(X_train.drop(columns="Year"))
+#X_train = pd.DataFrame(X_train_normalised)
+
+plt.plot(X)
+plt.show()
 
 from sklearn.model_selection import train_test_split
 
@@ -25,16 +41,29 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_s
 # train-validation split
 X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size=0.1, random_state=42)
 
-from sklearn.preprocessing import MinMaxScaler
+# TODO: create sequences
 
-# normalise data
-#   TODO: replace sklearn with tf
-#         retain year coloumn, months if possible
-scaler = MinMaxScaler()
-X_train_normalised = scaler.fit_transform(X_train.drop(columns="Year"))
-X_train = pd.DataFrame(X_train_normalised)
+'''
 
-X_train.plot()
-#plt.show()
+import tensorflow as tf
 
+# force GPU
+#with tf.device('/GPU:0'):
 
+# LSTM
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.LSTM(units=128, input_shape=(X_train.shape[0], X_train.shape[1])))
+model.add(tf.keras.layers.LSTM(units=64))
+model.add(tf.keras.layers.LSTM(units=64))
+model.add(tf.keras.layers.Dense(units=1))
+
+model.compile(loss='mean_square_error')
+
+model.fit(X_train, y_train, epochs=1, batch_size=1, verbose=1)
+
+plt.plot(history.history['loss'])
+plt.ylabel("loss")
+plt.xlabel("Epoch")
+plt.show()
+
+'''
