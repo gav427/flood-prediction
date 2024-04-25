9600f23aa90ae29adfe0ec5d1f8df589a60afd64
Gavin Chapman-Moore <gavin.chapmanmoore@gmail.com>
2024-04-25 16:29:45 +1000

    ./model.py: reshape data + LSTM model rewrite + train model with early stopping

--- src/model.py
+++ src/model.py
@@ -12,6 +12,9 @@ df_flood = df_flood[(df_flood.year <= 2021)]
 #   TODO: remove second 'year' column added by merging
 df = pd.merge(df_rain,df_flood, left_on='Year', right_on='year')
 
+# check shape
+print("Original shape:", df.shape)
+
 import matplotlib.pyplot as plt
 
 # visualise data
@@ -19,23 +22,46 @@ import matplotlib.pyplot as plt
 #df_flood.plot(x="year")
 #plt.show()
 
-# convert to arrays
-X = df[df.columns[:-12]].drop(columns=["Year","year"]).values #df_rain.drop(columns="Year").values
-y = df[df.columns[-12:]].values #df_flood.values
+# data-target split
+X = df[df.columns[:-12]].drop(columns=["Year","year"]) #df_rain.drop(columns="Year").values
+y = df[df.columns[-12:]] #df_flood.values
+
+# check shape after sampling target data
+print("X:",X.shape)
+print("y:",y.shape)
 
 ### TODO: should data be balanced here? How can mostly false (0) be balanced?
 
 from sklearn.preprocessing import MinMaxScaler
 
 # normalise data
-#   TODO: replace sklearn with tf
-#         retain year coloumn, months if possible
 scaler = MinMaxScaler()
-X = scaler.fit_transform(X) #scaler.fit_transform(X_train.drop(columns="Year"))
-#X_train = pd.DataFrame(X_train_normalised)
+X_scaled = scaler.fit_transform(X) #scaler.fit_transform(X_train.drop(columns="Year"))
+X = pd.DataFrame(X_scaled, columns=list(X.columns))
 
-plt.plot(X)
-plt.show()
+#plt.plot(X)
+#plt.show()
+
+import numpy as np
+
+# reshape data
+seq = 20
+dfX = []
+dfY = []
+for i in range(0,len(df) - seq):
+    data = []
+    for j in range(0,seq):
+        d = []
+        for col in df.columns:
+            d.append(df[col][i +j])
+        data.append(d)
+    dfX.append(data)
+    dfY.append(df[['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']].iloc[i + seq].values)
+X, y = np.array(dfX), np.array(dfY)
+
+# check shape after reshaping
+print("X:",X.shape)
+print("y:",y.shape)
 
 from sklearn.model_selection import train_test_split
 
@@ -45,9 +71,11 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_s
 # train-validation split
 X_train, X_val, y_train, y_val = train_test_split(X_train,y_train, test_size=0.1, random_state=42)
 
-# TODO: create sequences
+# check shape after splitting
+print("X_train:",X_train.shape,"X_test:",X_test.shape)
+print("y_train:",y_train.shape,"y_test:",y_test.shape)
+print("X_val:",X_val.shape,"y_val:",y_val.shape)
 
-'''
 
 import tensorflow as tf
 
@@ -56,18 +84,21 @@ import tensorflow as tf
 
 # LSTM
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.LSTM(units=128, input_shape=(X_train.shape[0], X_train.shape[1])))
-model.add(tf.keras.layers.LSTM(units=64))
-model.add(tf.keras.layers.LSTM(units=64))
-model.add(tf.keras.layers.Dense(units=1))
+model.add(tf.keras.layers.LSTM(units=128, input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(tf.keras.layers.Dropout(0.2)) # control overfitting
+model.add(tf.keras.layers.Dense(units=12,activation='sigmoid'))
+model.compile(loss='MeanSquaredError', optimizer='Adam')
+model.summary()
 
-model.compile(loss='mean_square_error')
+#print(X_train.shape[1])
 
-model.fit(X_train, y_train, epochs=1, batch_size=1, verbose=1)
+# enable early stopping
+cb = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+
+# training
+history = model.fit(X_train, y_train, epochs=20, batch_size=1, verbose=1, callbacks=[cb])
 
 plt.plot(history.history['loss'])
 plt.ylabel("loss")
 plt.xlabel("Epoch")
 plt.show()
-
-'''
